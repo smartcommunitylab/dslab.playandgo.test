8d8a1909c7e123b8d2c1950d8fa094743ae198fd
Mauro-Zanotti <mzanotti@fbk.eu>
2024-11-26 13:20:23 +0100

    Fixed Swagger on ApiController. Added OpenAPI configuration.

--- src/main/java/eu/fbk/dslab/playandgo/test/ApiController.java
+++ src/main/java/eu/fbk/dslab/playandgo/test/ApiController.java
@@ -1,13 +1,7 @@
 package eu.fbk.dslab.playandgo.test;
 
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.headers.Header;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,33 +11,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import eu.fbk.dslab.playandgo.test.hereapi.HereAPITestManager;
 
-import javax.validation.Valid;
-
 @RestController
 public class ApiController {
 	@Autowired
 	HereAPITestManager hereTestManager;
 
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200",
-					description = "Successful", content = @Content(schema = @Schema(implementation = String.class))),
-			@ApiResponse(responseCode = "400", description = "Invalid Parameters", content = @Content) })
 	@GetMapping("/api/test/track/send")
 	public ResponseEntity<String> sendTrack(
-			@NotNull @Parameter(description = "Starting Date and Time of the track. Format: \"yyyy-MM-dd'T'HH:mm:ss\"")
-				@Valid @RequestParam(value = "date") String date,
-			@NotNull @Parameter(description = "Latitude and Longitude of the origin point. Format example: \"01.0000,01.0000\"")
-				@Valid @RequestParam(value = "origin") String origin,
-			@NotNull @Parameter(description = "Latitude and Longitude of the destination point. Format example: \"01.0000,01.0000\"")
-				@Valid @RequestParam(value = "destination") String destination,
-			@NotNull @Parameter(description = "Transport Mode. Possible values: walk, bike, car, train, bus.")
-				@Valid() @RequestParam(value = "mean", required = false, defaultValue = "") String mean,
+			@Parameter(description = "Starting Date and Time of the track.", example = "2024-11-20T13:00:00")
+				@RequestParam(value = "date") String date,
+			@Parameter(description = "Latitude and Longitude of the origin point.", example="46.0659,11.1545")
+				@RequestParam(value = "origin") String origin,
+			@Parameter(description = "Latitude and Longitude of the destination point.", example = "46.0342,11.1314")
+				@RequestParam(value = "destination") String destination,
+			@Parameter(description = "Transport Mode. Possible values: walk, bike, car, train, bus or empty(any transport mode).")
+				@RequestParam(value = "mean", required = false) String mean,
 			@Parameter(description = "Assign user to Survey. Use false.")
-				@RequestParam(value = "assignSurvey", required = false, defaultValue = "false") boolean assignSurvey,
+				@RequestParam(value = "assignSurvey", required = false) boolean assignSurvey,
 			@Parameter(description = "Invite player to challenge. Use false.")
-				@RequestParam(value = "invitePlayer", required = false, defaultValue = "false") boolean invitePlayer,
-			@Parameter(description = "Choose if you want the result to be Multimodal (multimodal = true) or just one Polyline of the first section (multimodal=false)")
-				@RequestParam(value = "multimodal", required = false, defaultValue = "true") boolean multimodal
+				@RequestParam(value = "invitePlayer", required = false) boolean invitePlayer,
+			@Parameter(description = "Multimodal (multimodal = true) or Polyline of the first section (multimodal=false)")
+				@RequestParam(value = "multimodal", required = false) boolean multimodal
 	) throws Exception {
 		if(!multimodal) {
 			if(StringUtils.isEmpty(mean)) {
--- src/main/java/eu/fbk/dslab/playandgo/test/hereapi/config/HereAPIConfiguration.java
+++ src/main/java/eu/fbk/dslab/playandgo/test/hereapi/config/HereAPIConfiguration.java
@@ -5,9 +5,36 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+
 @Configuration
 public class HereAPIConfiguration {
-    
+
+    @Bean
+    public OpenAPI springOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info().title("Play&Go Project")
+                        .version("2.0.1")
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SmartCommunityLab")
+                        .url("https://www.smartcommunitylab.it/"))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName));
+    }
     /**
      * A Spring RestTemplate for making HTTP requests to the HERE API.
      *
